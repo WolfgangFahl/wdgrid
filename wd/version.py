f44cab3fdb02ce4c5ef82070c86a1ae5d14c9e94
Wolfgang Fahl <wf@bitplan.com>
2025-10-30 10:29:38 +0100

    upgrades deps

--- wd/version.py
+++ wd/version.py
@@ -19,7 +19,7 @@ class Version(object):
     version = wd.__version__
     description = "wikdata grid and sync"
     date = "2021-12-12"
-    updated = "2025-06-19"
+    updated = "2025-10-30"
 
     authors = "Wolfgang Fahl"
 
